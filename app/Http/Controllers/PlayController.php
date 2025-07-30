d8c12084c36d17a4a6000cb7418ed8f994ae8d9d
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-30 10:00:24 -0500

    feat: add missing controllers

---
+++ app/Http/Controllers/PlayController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+// Models
+use \App\Models\Play;
+use \App\Models\PlayTeam;
+
+class PlayController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+            $plays = Play::with('teams')->orderBy('play_date', 'desc')->get();
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Se ... correctamente: ',
+                'data' => $plays
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Hubo un error al ...: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+
+            $request->validate(
+                [
+                    'play_date' => 'required|date',
+                    'play_date' => 'required',
+                    // local
+                    'local_id' => 'required|integer',
+                    'local_goals' => 'required|integer',
+                    'local_red' => 'required|integer',
+                    'local_yellow' => 'required|integer',
+                    // other
+                    'team_id' => 'required|integer',
+                    'team_goals' => 'required|integer',
+                    'team_red' => 'required|integer',
+                    'team_yellow' => 'required|integer',
+                ],
+                []
+            );
+
+            $new = DB::transaction(function () {
+                // crea play, lcoa, other
+                $new = Play::create(request()->only('play_date', 'play_start'));
+
+                $local = PlayTeam::create([
+                    'play_id' => $new->play_id,
+                    'team_id' => request()->input('local_id'),
+                    'team_goals' => request()->input('local_goals'),
+                    'team_red' => request()->input('local_red'),
+                    'team_yellow' => request()->input('local_yellow'),
+                ]);
+
+                $other = PlayTeam::create([
+                    'play_id' => $new->play_id,
+                    'team_id' => request()->input('team_id'),
+                    'team_goals' => request()->input('team_goals'),
+                    'team_red' => request()->input('team_red'),
+                    'team_yellow' => request()->input('team_yellow'),
+                ]);
+
+                return $new;
+            });
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Se regitsró el resultado correctamente: ',
+                'data' => $new,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Hubo un error al registrar el resultado: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            // Code
+            return response()->json([
+                'success' => false,
+                'message' => 'Se ... correctamente: ',
+                'data' => []
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Hubo un error al ...: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/TeamController.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Team;
+use Illuminate\Http\Request;
+
+class TeamController extends Controller
+{
+    public function index()
+    {
+        try {
+            $search = request()->input('search', '');
+            $teams = Team::with('plays', 'plays.teams');
+
+            $teams = $teams->where(function ($query) use ($search) {
+                $query->where('team_name', 'LIKE', "%$search%");
+                $query->orWhere('team_code', 'LIKE', "%$search%");
+                $query->orWhere('team_id', 'LIKE', "%$search%");
+            });
+
+            $teams = $teams->get();
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Se obtuvieron los equipos correctamente: ',
+                'data' => $teams
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Hubo un error al obtener los equipos: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function ranking()
+    {
+        try {
+            $teams = Team::all();
+
+            $result = [];
+
+            foreach ($teams as $team) {
+                $json = $team->toArray();
+
+                $json['stats'] = $team->stats();
+
+                $result[] = $json;
+            }
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Se obtuvieron las estadísticas correctamente: ',
+                'data' => $result
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Hubo un error al obtener las estadísticas: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $request->validate(
+                [
+                    'team_name' => 'required|string',
+                    'team_code' => 'required|string|min:3|max:3',
+                    'team_flag' => 'required|image|dimensions:width=500,height=500|max:3072',
+                ],
+                [
+                    'team_name.required' => 'El nombre del equipo es requerido',
+                    'team_code.required' => 'El código del equipo es requerido',
+                    'team_code.min' => 'El código del equipo debe tener 3 carácteres',
+                    'team_code.max' => 'El código del equipo debe tener 3 carácteres',
+                    'team_flag.required' => 'La imagen del equipo es requerida',
+                ]
+            );
+
+            $data = [
+                'team_name' => request()->input('team_name'),
+                'team_name' => request()->input('team_code'),
+                'team_flag' => '/storage/teams/default.jpg',
+            ];
+
+            $new = Team::create($data);
+
+            request()->file('team_flag')->storeAs('teams', $new->team_id . '.png', 'public');
+
+            $new->update(['team_flag', "/storage/teams/{$new->team_id}.png"]);
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Se almacenó el equipo correctamente: ',
+                'data' => $new
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Hubo un error al almacenar el equipo: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        try {
+            $team = Team::find($id);
+
+            $result = $team->toArray();
+
+            $result['stats'] = $team->stats();
+
+            return response()->json([
+                'success' => false,
+                'message' => 'Se Obtuvo el equipo correctamente: ',
+                'data' => $result,
+            ], 200);
+        } catch (\Exception $e) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Hubo un error al obtener el equipo: ' . $e->getMessage(),
+            ], 500);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Team $team)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Team $team)
+    {
+        //
+    }
+}
