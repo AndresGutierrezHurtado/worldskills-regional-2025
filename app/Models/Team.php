7953501f3f4f20b4cfdce5893e9f76967737b79c
AndresGutierrezHurtado <andres52885241@gmail.com>
2025-07-30 09:38:06 -0500

    feat: add stats logic

--- app/Models/Team.php
+++ app/Models/Team.php
@@ -22,4 +22,44 @@ class Team extends Model
         return $this->belongsToMany(Team::class, 'play_teams', 'play_id', 'team_id')
             ->withPivot('team_local', 'team_goals', 'team_red', 'team_yellow');
     }
+
+    public function stats()
+    {
+        $stats = [
+            'points' => 0,
+            'total_played' => 0,
+            'win' => 0,
+            'drawn' => 0,
+            'lost' => 0,
+            'goals_for' => 0,
+            'goals_against' => 0,
+            'goals_difference' => 0,
+        ];
+
+        $plays = $this->plays;
+
+        foreach ($plays as $play) {
+            $current = $play->teams->where('team_id', $this->team_id)->first();
+            $other = $play->teams->where('team_id', '!=', $this->team_id)->first();
+
+            // Goals
+            $stats['goals_for'] += $current->pivot->team_goals;
+            $stats['goals_against'] += $other->pivot->team_goals;
+
+            // Matches
+            if ($current->pivot->team_goals === $other->pivot->team_goals) {
+                $stats['drawn'] += 1;
+            } else if ($current->pivot->team_goals > $other->pivot->team_goals) {
+                $stats['win'] += 1;
+            } else {
+                $stats['lost'] += 1;
+            }
+        }
+
+        $stats['total_played'] = $stats['win'] + $stats['lost'] + $stats['drawn'];
+        $stats['points'] = ($stats['win'] * 3) + $stats['drawn'];
+        $stats['goals_difference'] = $stats['goals_for'] - $stats['goals_difference'];
+
+        return $stats;
+    }
 }
